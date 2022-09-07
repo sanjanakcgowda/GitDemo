889221f72543a10cf9aa4bc77f89ea61263ff574
sanjana <sanjanakc2020@gmail.com>
2022-09-07 12:05:54 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndriod()
+	{
+		System.out.println("NativeAppAndriod");
+	}
+
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+}
